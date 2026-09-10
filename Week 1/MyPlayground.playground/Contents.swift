645b69f7608b12c9f03a1110ce9e59332f9e5ecd
eshlovescode <emd9533@nyu.edu>
2026-09-10 16:01:01 -0400

    hw

--- Week 1/MyPlayground.playground/Contents.swift
+++ Week 1/MyPlayground.playground/Contents.swift
@@ -1,3 +1,18 @@
-import UIKit
+import Foundation
+import string_h
 
-var greeting = "Hello, playground"
+let response = "🤪😭🥹👀🫀🎶🍆🍑🧿💐🚭"
+let words = ["OMG","LOL","smh","Lmao","AI psychosis","ngl","ILY"]
+var counter = 0
+
+print("Hi, I missed you! How are you honey?", terminator: "\n\t\t\t\t\t\t ")
+
+while counter < 6{
+    var str = ""
+    for _ in 0..<words.count {
+        str.append(response.randomElement()!)
+    }
+    print(str)
+    counter+=1
+}
+
