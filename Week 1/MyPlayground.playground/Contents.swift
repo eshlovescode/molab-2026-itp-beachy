8902065a2cc3040d259f221c78607446f03bcdd3
eshlovescode <emd9533@nyu.edu>
2026-09-17 11:28:28 -0400

    Commits

--- Week 1/MyPlayground.playground/Contents.swift
+++ Week 1/MyPlayground.playground/Contents.swift
@@ -1,18 +1,71 @@
-import Foundation
-import string_h
+//import Foundation
+//import string_h
+//
+//let response = "🤪😭🥹👀🫀🎶🍆🍑🧿💐🚭"
+//let words = ["OMG","LOL","smh","Lmao","AI psychosis","ngl","ILY"]
+//var counter = 0
+//
+//print("Hi, I missed you! How are you honey?", terminator: "\n\t\t\t\t\t\t ")
+//
+//while counter < 6{
+//    var str = ""
+//    for _ in 0..<words.count {
+//        str.append(response.randomElement()!)
+//    }
+//    print(str)
+//    counter+=1
+//}
 
-let response = "🤪😭🥹👀🫀🎶🍆🍑🧿💐🚭"
-let words = ["OMG","LOL","smh","Lmao","AI psychosis","ngl","ILY"]
-var counter = 0
+//This is the video recap
+// constants and variables
+var name = "Ted" //mutable
+name = "Rebecca"
+let user = "Daphne" //constant
+print(user)
 
-print("Hi, I missed you! How are you honey?", terminator: "\n\t\t\t\t\t\t ")
+// backslash for taking the string in
+let quote = "He walked away from the sign that read \"Believe\" and walked away."
+//To write multiple lines we have triple quotes
+let line = """
+I wanted a lot of sthings like
+food, water,
+shelter,
+clothes,
+a good economy
+"""
+//String functions
+print(quote.hasPrefix("He")) //beginning of string and the next line is end of string
+print(quote.hasSuffix("Away.")) //case sensitive
 
-while counter < 6{
-    var str = ""
-    for _ in 0..<words.count {
-        str.append(response.randomElement()!)
-    }
-    print(str)
-    counter+=1
-}
+//Whole numbers as given as int
+let score = 10
+let higherScore = score + 10
+let halvedScore = score/2
 
+var counter = 10
+counter+=5 //compund assignment statements
+
+//mathematical operations
+let number = 120
+print(number.isMultiple(of:3))
+let id = Int.random(in:1...1000)
+//other data types are : Boolean, Double (known as float or double in C++),
+//we can use a toggle function on Boolean variables. variablename.toggle()
+
+//String interpolation
+name = "Eshaan"
+let age = "27"
+let message = "My name is \(name) and my age is \(age)"
+print(message)
+
+//Arrays of different types
+var colors = ["Red","Green","Blue"]
+let numbers = [4, 8, 15, 16]
+var readings = [0.1, 0.2, 0.3]
+
+print(colors[0])
+print(numbers[2])
+
+colors.append("Magenta")
+colors.remove(at: 0)  //function to pop the value out of index 0
+print(colors.count)
